473208301c7bb6544facd3a2852212e1dba04f73
redolf250 <55606159+redolf250@users.noreply.github.com>
2023-12-25 11:19:00 +0000

    Model to keep track of crawled job ids

---
+++ Model/GhanaJobId.cs
@@ -0,0 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Crawler;
+
+public class GhanaJobId
+{
+    [Key]
+    public int Id {get; set;}
+    public string JobId { get; set; } = String.Empty;
+}
