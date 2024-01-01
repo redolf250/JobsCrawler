85aca33c25c8ee98c2d272ad340e226ddfa26db0
redolf250 <55606159+redolf250@users.noreply.github.com>
2024-01-01 14:03:04 +0000

    setting db set to null

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,11 +1,12 @@
+using Crawler.Model;
 using Microsoft.EntityFrameworkCore;
 
 namespace Crawler.Data;
 
-public class ApplicationDbContext  : DbContext
+public class ApplicationDbContext : DbContext
 {
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
 
-    public DbSet<CrawledJob> CrawledJobs { get; set; }
-    public DbSet<GhanaJobId> GhanaJobIds { get; set; }
+    public DbSet<CrawledJob>? CrawledJobs { get; set; }
+    public DbSet<GhanaJobId>? GhanaJobIds { get; set; }
 }
