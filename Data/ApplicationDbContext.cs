278289d3b7f530326e9485e411123ec136157e1c
redolf250 <55606159+redolf250@users.noreply.github.com>
2024-01-09 08:33:51 +0000

    dbset renamed

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -7,6 +7,6 @@ public class ApplicationDbContext : DbContext
 {
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
 
-    public DbSet<CrawledJob>? CrawledJobs { get; set; }
-    public DbSet<GhanaJobId>? GhanaJobIds { get; set; }
+    public DbSet<CrawledJob>? Staging_GhanaJob { get; set; }
+    public DbSet<GhanaJobId>? Staging_GhanaJobIds { get; set; }
 }
