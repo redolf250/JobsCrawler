0e07e8c50b890d3bf975213cbf51d5864a9748b2
redolf250 <55606159+redolf250@users.noreply.github.com>
2024-01-09 08:30:50 +0000

    some fields dropped from model class

--- Model/CrawledJob.cs
+++ Model/CrawledJob.cs
@@ -4,28 +4,19 @@ namespace Crawler.Model;
 
 public class CrawledJob
 {
-    [Key] public Guid CrawledJobId { get; set; }
-
+    [Key] public Guid UUId { get; set; }
     public string? JobId { get; set; }
     public string? JobTitle { get; set; }
     public string? JobSourceUrl { get; set; }
     public string? JobDescription { get; set; }
     public string? JobRequirement { get; set; }
-
     public string? EmploymentType { get; set; }
     public string? AccademicQualification { get; set; }
     public string? YearsOfExperience { get; set; }
     public string? Location { get; set; }
+    public string? EmployerName { get; set; }
+    public DateTime? CreatedAt { get; set; }
+
     public string? Salary { get; set; }
     public string? Currency { get; set; }
-    public string? EmployerName { get; set; }
-    public char? Status { get; set; }
-    public int? SubSectorId { get; set; }
-    public int? CountryId { get; set; }
-    public int? RegionStateId { get; set; }
-    public int? DistrictId { get; set; }
-    public int? WorkPlaceTypeId { get; set; }
-    public bool? IsDeclined { get; set; }
-    public DateTime? EntryDate { get; set; }
-    public DateTime? CreatedAt { get; set; }
 }
